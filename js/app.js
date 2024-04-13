41e4fc15d50ee14ca97b5392042268d983c97e91
qdbp <me@qdbp.codes>
2024-04-13 09:56:10 +0200

    🌱 add webgpu basics

--- js/app.js
+++ js/app.js
@@ -14,16 +14,70 @@ context.configure({
     format: canvasFormat,
 })
 
+const GRID_SIZE = 8
+
 const mapVertices = new Float32Array([
     // x, y
-    -0.5,  0.5,
-     0.5,  0.5,
-    -0.5, -0.5,
+    -0.8, -0.8, // Triangle 1
+    0.8, -0.8,
+    0.8,  0.8,
 
-     0.5,  0.5,
-    -0.5,  0.5,
-    -0.5, -0.5,
+    -0.8, -0.8, // Triangle 2
+    0.8,  0.8,
+    -0.8,  0.8,
 ])
+
+const vertexBuffer = device.createBuffer({
+    label: "Cell vertices",
+    size: mapVertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+})
+device.queue.writeBuffer(vertexBuffer, 0, mapVertices)
+
+const cellShader = device.createShaderModule({
+    label: "Cell Shader",
+    code: `
+        
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+            return vec4f(pos, 0, 1);
+        }
+        
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f {
+            return vec4f(1, 0, 0, 1);
+        }
+    `,
+})
+
+const vertexBufferLayout = {
+    arrayStride: 8,
+    attributes: [{
+        format: "float32x2",
+        offset: 0,
+        shaderLocation: 0,
+    }],
+}
+
+const renderPipeline = device.createRenderPipeline({
+    label: "Render Pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShader,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+    },
+    fragment: {
+        module: cellShader,
+        entryPoint: "fragmentMain",
+        targets: [{
+            format: canvasFormat
+        }]
+    },
+})
+
+
+
 
 const encoder = device.createCommandEncoder()
 
@@ -35,14 +89,10 @@ const pass = encoder.beginRenderPass({
         storeOp: "store",
     }]
 })
+
+pass.setPipeline(renderPipeline)
+pass.setVertexBuffer(0, vertexBuffer)
+pass.draw(mapVertices.length / 2)
 pass.end()
-
-const vertexBuffer = device.createBuffer({
-    label: "Cell vertices",
-    size: mapVertices.byteLength,
-    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-})
-
-device.queue.writeBuffer(vertexBuffer, 0, mapVertices)
 
 device.queue.submit([encoder.finish()])
